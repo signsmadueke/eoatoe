95d5e926186dd5a2e892a93d11eac91d33badcdc
godson-pius <40927597+godson-pius@users.noreply.github.com>
2021-07-21 07:27:24 +0100

    Fixed the add counselling

--- admin/add_counselling.php
+++ admin/add_counselling.php
@@ -54,7 +54,7 @@ blockUrlHackers();
                                         <div class="col-md-6">
                                             <div class="form-group">
                                                 <label for="date">Date Added</label>
-                                                <input type="text" id="date" value="<?= date("Y-m-d")?>" class="form-control" name="datePosted">
+                                                <input type="text" readonly id="date" value="<?= date("Y-m-d")?>" class="form-control" name="datePosted">
                                             </div>
                                         </div>
                                     </div>
